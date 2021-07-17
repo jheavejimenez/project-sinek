00e89f76b86af38afecdaf701e63f2dffc7bbb4c
rsiton18 <robin.siton@rocketmail.com>
2021-07-17 18:34:22 +0800

    refractor member route

--- backend/routes/members.js
+++ backend/routes/members.js
@@ -1,39 +1,41 @@
 const router = require('express').Router();
+const Members = require('../models/members.model');
 let Member = require('../models/members.model');
 
-router.route('/').get((req, res) => {
-    Member.find()
-    .then(member => res.json(member))
+router.route('/').get(async (req, res) => {
+    try {
+        const members = await Members.find();
+        res.json(members)
+
+    } catch (err) {
+        res.status(400).json('Error: ' + err);
+    }
+   
+}).post ((req, res) => {
+    const newMember = new Member(req.body);
+    newMember.save()
+    .then(() => res.json('Member added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// ADD
-router.route('/add').post((req, res) => {
-  const newMember = new Member(req.body);
-
-  newMember.save()
-  .then(() => res.json('Member added!'))
-  .catch(err => res.status(400).json('Error: ' + err));
-});
-
-// DELETE
-router.route('/delete/:id').delete((req, res) => {
-    Member.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Evaluation deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-// UPDATE
-router.route('/update/:id').post((req, res) => {
+router.put('/:id', async(req, res) => {
     Member.findById(req.params.id)
     .then(member => {
         member.username = req.body.username;
 
         member.save()
-        .then(() => res.json('Evaluation updated!'))
+        .then(() => res.json('Member updated!'))
         .catch(err => res.status(400).json('Error: ' + err));
     })
     .catch(err => res.status(400).json('Error: ' + err));
+}).delete(async (req, res) => {
+    try {
+        await Member.findByIdAndDelete(req.params.id);
+        res.json('Member Deleted!');
+
+    } catch(err) {
+        res.status(400).json('Error: ' + err);
+    }
 });
 
 module.exports = router;
