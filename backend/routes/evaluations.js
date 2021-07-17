fcacb5c9d6a8c9d4260dbe3b3e8a31bbb6c5d6e7
rsiton18 <robin.siton@rocketmail.com>
2021-07-17 17:03:18 +0800

    refractor

--- backend/routes/evaluations.js
+++ backend/routes/evaluations.js
@@ -3,82 +3,48 @@ const router = require('express').Router();
 let Evaluation = require('../models/evaluation.model');
 let Member = require('../models/members.model');
 
+router.route('/').get(async (req, res) => {
+   try {
+     const evaluation = await Evaluation.find();
+     res.json(evaluation)
 
-router.route('/').get((req, res) => {
-    Evaluation.find()
-    .then(evaluation => res.json(evaluation))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
+   } catch (err) {
+    res.status(400).json('error')
 
-// ADD
-router.route('/add').post(async (req, res) => {
-  try {
-    const newEvaluation = new Evaluation({
-      managementEmail: req.body.managementEmail,
-      teamName: req.body.teamName,
-      members: []
-    });
-    
-    const members = req.body.members;
-    
-    members.forEach(async member => {
-      const newMember = new Member(member);
-      // Member.findOne({memberEmail: memberEmail.email}).then(function(result){
-      //     return result !== null;
-      // });
+   }
+}).post ((req, res) => {
+   try {
+     const newEvaluation = new Evaluation({
+       managementEmail: req.body.managementEmail,
+       teamName: req.body.teamName,
+       members: []
 
-      newEvaluation.members.unshift({memberId: newMember._id});
-    
-      await newMember.save();
-      await newEvaluation.save();
-    });
-        
-    res.json(members);
-    
-  } catch(err) {
-    res.status(400).json('Error: ' + err);
-  }
-
-
-  // const newEvaluation = new Evaluation(req.body);
-
-  // newEvaluation.save()
-  // .then(evaluation => {
-  //   try {
-  //     const members = req.body.members;
-  
-  //     members.forEach(async member => {
-  //      const newMember = new Member(member);
-  //      newEvaluation.members.unshift({memberId: newMember._id});
+     });
+     
+     const members = req.body.members;
+     
+     members.forEach(async member => {
+       const newMember = new Member(member);
+       // Member.findOne({memberEmail: memberEmail.email}).then(function(result){
+       //     return result !== null;
+       // });
+ 
+       newEvaluation.members.unshift({memberId: newMember._id});
+     
+       await newMember.save();
+       await newEvaluation.save();
        
-  //      await newMember.save();
-  //      await newEvaluation.save();
-  //     });    
-  
-  //     res.json(members);
-  //   } catch(err) {
-  //     res.status(400).json('Error: ' + err);
-  //   }
-  // })
-  // .catch(err => res.status(400).json('Error: ' + err));
+    });
+     res.json(members);
+     
+   } catch(err) {
+     res.status(400).json('Error: ' + err);
+ 
+   }
+
 });
 
-// READ Specific collection
-router.route('/:id').get((req, res) => {
-    Evaluation.findById(req.params.id)
-    .then(evaluation => res.json(evaluation))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-// DELETE
-router.route('/:id').delete((req, res) => {
-    Evaluation.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Evaluation deleted.'))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-// UPDATE
-router.put('/update/:id', async(req, res) => {
+router.put('/:id', async(req, res) => {
   try {
     const update = {
       title: req.body.title,
@@ -87,25 +53,19 @@ router.put('/update/:id', async(req, res) => {
 
     const evaluation = await Evaluation.findByIdAndUpdate(req.params.id, update, { new: true });
     res.json(evaluation);
-  }catch(error){
+
+  } catch(error) {
     res.status(400).json('Error: ' + err);
   }
 
+}).delete(async (req, res) => {
+  try {
+    await Evaluation.findByIdAndDelete(req.params.id);
+    res.json('Evaluation Deleted!');
 
-
-
-
-
-    // Evaluation.findById(req.params.id)
-    // .then(evaluation => {
-    //   evaluation.title = req.body.title;
-    //   evaluation.members = req.body.members;
-
-    //   evaluation.save()
-    //     .then(() => res.json('Evaluation updated!'))
-    //     .catch(err => res.status(400).json('Error: ' + err));
-    // })
-    // .catch(err => res.status(400).json('Error: ' + err));
+  } catch (err) {
+    res.status(400).json('Error: ' + err);
+  }
 });
 
 module.exports = router;
---
+++ sinek/src/repository/EvaluationRepository.js
@@ -0,0 +1,38 @@
+import * as axios from "axios";
+import ApiConfig from "./ApiConfig"
+
+export async function createEvaluation(
+    managementEmail,
+    teamName,
+    evaluationLink,
+    members,
+
+) {
+    const data = {managementEmail, teamName, evaluationLink, members,}
+    console.log(data);
+
+    return await axios.post(`${ApiConfig.url}/api/evaluations`, data);
+}
+
+export async function deleteEvaluation(
+    id,
+
+  ) {
+    return await axios.delete(`${ApiConfig.url}/api/evaluations/${id}`);
+  }
+  
+  
+  export async function updateEvaluation(
+    id,
+    managementEmail,
+    teamName,
+    evaluationLink,
+    members,
+
+  ) {
+    const data = {managementEmail, teamName, evaluationLink, members,}
+    console.log(data);
+  
+    return await axios.put(`${ApiConfig.url}/api/evaluations/${id}`, data);
+  }
+  
