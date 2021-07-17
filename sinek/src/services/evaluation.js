facdb4fd254898a40e9ff3ff1264a71cfc806a6f
rsiton18 <robin.siton@rocketmail.com>
2021-07-17 14:33:57 +0800

    refractor

--- sinek/src/services/evaluation.js
+++ sinek/src/services/evaluation.js
@@ -1,17 +1,17 @@
-import http from "../http-common";
+// import http from "../http-common";
 
-class EvaluationDataService {
-    getAll() {
-        return http.get("/");
-    }
+// class EvaluationDataService {
+//     getAll() {
+//         return http.get("/");
+//     }
 
-    createEvaluation(data) {
-        return http.post("/add", data);
-    }
+//     createEvaluation(data) {
+//         return http.post("/add", data);
+//     }
 
-    updateEvaluation(id) {
-        return http.post(`/update/${id}`);
-    }
-}
+//     updateEvaluation(id) {
+//         return http.post(`/update/${id}`);
+//     }
+// }
 
-export default new EvaluationDataService();
+// export default new EvaluationDataService();
