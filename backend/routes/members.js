33b1dcb693690483e59d692d06a87c314a7512c6
rsiton18 <robin.siton@rocketmail.com>
2021-07-17 18:40:33 +0800

    refractor member route

--- backend/routes/members.js
+++ backend/routes/members.js
@@ -13,13 +13,15 @@ router.route('/').get(async (req, res) => {
    
 }).post ((req, res) => {
     const newMember = new Member(req.body);
+
     newMember.save()
     .then(() => res.json('Member added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.put('/:id', async(req, res) => {
+router.route('/:id').put(async (req, res) =>{
     Member.findById(req.params.id)
+
     .then(member => {
         member.username = req.body.username;
 
@@ -28,6 +30,7 @@ router.put('/:id', async(req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
     })
     .catch(err => res.status(400).json('Error: ' + err));
+
 }).delete(async (req, res) => {
     try {
         await Member.findByIdAndDelete(req.params.id);
